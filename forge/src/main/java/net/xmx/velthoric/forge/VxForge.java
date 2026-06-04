9ad5242866f57bee8c4fb8fe267bc34004893e17
xI-Mx-Ix <xmxv.wz@gmail.com>
2026-06-04 23:05:16 +0200

    Repair Forge 1.20.1 configuration and add missing dependencies

---
+++ forge/src/main/java/net/xmx/velthoric/forge/VxForge.java
@@ -0,0 +1,30 @@
+/*
+ * This file is part of Velthoric.
+ * Licensed under LGPL 3.0.
+ */
+package net.xmx.velthoric.forge;
+
+import dev.architectury.platform.Platform;
+import dev.architectury.platform.forge.EventBuses;
+import dev.architectury.utils.Env;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.xmx.velthoric.init.VxMainClass;
+
+/**
+ * Main class for Forge integration.
+ * <p>
+ * Initializes the mod and handles client-side initialization if running on the client.
+ *
+ * @author xI-Mx-Ix
+ */
+@Mod(VxMainClass.MODID)
+public final class VxForge {
+    public VxForge() {
+        EventBuses.registerModEventBus(VxMainClass.MODID, FMLJavaModLoadingContext.get().getModEventBus());
+        VxMainClass.onInit();
+        if (Platform.getEnvironment() == Env.CLIENT) {
+            VxMainClass.onClientInit();
+        }
+    }
+}
